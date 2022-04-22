c1d982cc8f0834f515e54654573ad1c3d2072361
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-04-21 20:40:16 -0500

    Add final_practice and self_eval

--- final_practice.rb
+++ final_practice.rb
@@ -1,38 +1,75 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+def greeting
+  puts "Hello there!"
+end
 
+greeting
+
+# What is the return value of your method?
+#The return value is nil because puts is on the last line of the method.
+
+# How many arguments did you pass your method?
+# My method did not pass any arguments
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
 
+def custom_greeting(name)
+  puts "Hey there, #{name}!"
+end
+
+# test the method by specifying the argument as "Alex"
+custom_greeting("Alex")
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+#The return value is nil because puts is on the last line of the method.
 
+# How many arguments did you pass your method?
+# I passed one argument through my method
+
+# What data type was your argument(s)?
+# My argument was a string
 
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
 
+def square(num)
+  num * num
+end
+
+# test the method by specifying the argument as 2
+puts square(2)
 
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# The return value is 4
 
+# How many arguments did you pass your method?
+# I passed one argument through my method
+
+# What data type was your argument(s)?
+# My argument was an integer
 
 
 
 #4: Write a method named full name that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
 
+def full_name(first, middle, last)
+  puts first + " " + middle + " " + last
+end
 
+# test the method by specifying the argument
+full_name("Carlos", "John", "Stich")
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# nil
 
+# How many arguments did you pass your method?
+# three
+
+# What data type was your argument(s)?
+# strings 
--- self_eval.md
+++ self_eval.md
@@ -1,5 +1,10 @@
 # Self Evaluation
 
 - How confident do you feel about your use of built-in Ruby methods?
+I feel ok about using built-in methods. I can google it if I'm not familiar and figure it out.
+
 - How confident do you feel writing simple methods of your own?
+I am NOT confident with this part. I'm very confused about puts vs. prints vs. not using puts at all.
+
 - What questions do you have about methods, and what they are capable of? (ask a question!!!)
+When do I use "puts" to call on a method? Why can't I use "puts" to call on a method I've defined earlier if the definition of the method includes "puts"? What's the difference between something a method "prints" and something a method "returns"? 
